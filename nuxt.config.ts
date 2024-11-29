422dd8ffca701959f5db20f87328ad59597d9958
bl3ndd <evarzin@viasattech.ru>
2024-11-29 21:05:38 +0400

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/fonts/fonts.css'],
-  ssr: true,
+  ssr: false,
+  devServer: {
+    port: 3001
+  },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+    }
+  }
 })
